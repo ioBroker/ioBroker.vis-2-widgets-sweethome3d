97c107ed1a4d9b9e93f29de8bcba1d443da04dde
GermanBluefox <dogafox@gmail.com>
2023-11-15 16:43:38 +0100

    Added sync sources task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,17 @@
 const gulp = require('gulp');
 const adapterName = require('./package.json').name.replace('iobroker.', '');
 const gulpHelper = require('@iobroker/vis-2-widgets-react-dev/gulpHelper');
+const decompress= require('decompress');
+const fs = require('fs');
+
+gulp.task('update-viewer-source', async () => {
+    await decompress(`${__dirname}/viewerSources/SweetHome3DJSViewer-7.2.zip`, `${__dirname}/viewerSources/src`);
+    const files = fs.readdirSync(`${__dirname}/viewerSources/src/lib`);
+    // copy all files
+    for (const file of files) {
+        fs.writeFileSync(`src-widgets/src/lib/${file.replace(/\.js$/, '.txt')}`, fs.readFileSync(`${__dirname}/viewerSources/src/lib/${file}`));
+    }
+});
 
 // http://127.0.0.1:18082/vis-2-beta/widgets/vis-2-widgets-material/static/js/node_modules_iobroker_vis-2-widgets-react-dev_index_jsx-_adb40.af309310.chunk.js
 
